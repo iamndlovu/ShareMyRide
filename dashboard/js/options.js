a37560165b6a58eccdbbcb956ea84fbe1d44d394
iamndlovu <ndlovu.p@protonmail.com>
2021-02-09 13:23:57 +0200

    js functions to add all locations to dropdowns on load

---
+++ dashboard/js/options.js
@@ -0,0 +1,15 @@
+import locations from "./locations.js";
+
+window.addEventListener('load', () =>
+	{
+		const selectElements = document.querySelectorAll('select.location');
+
+		selectElements.forEach(element => {
+			for(let location of locations) {
+				const option = document.createElement('option');
+				option.value = location.toLowerCase();
+				option.textContent = location;
+				element.appendChild(option);
+			}
+		});
+	});
