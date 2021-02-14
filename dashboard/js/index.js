c574d839e3e1c1ecdd1b2d078b5bbc132af1fd51
iamndlovu <ndlovu.p@protonmail.com>
2021-02-14 18:59:58 +0200

    Add back button to drve and ride sections

--- dashboard/js/index.js
+++ dashboard/js/index.js
@@ -3,6 +3,12 @@ import toggleViews from './toggleViews.js';
 
 window.addEventListener('load', loadOptions);
 
+//show drive / ride section
 document.querySelectorAll('.options > .option').forEach(option => option.addEventListener('click', () => {
   option.classList.contains('drive') ? toggleViews('drive') : toggleViews('ride');
+}));
+
+//go back to home section
+document.querySelectorAll('i.back-home').forEach(arrow => arrow.addEventListener('click', () => {
+  toggleViews('home')
 }));
